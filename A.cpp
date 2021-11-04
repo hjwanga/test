9108e905d6bd1940231113ce9a74909578550d0f
David Wang <david_wang@phison.com>
2021-11-04 12:01:54 +0800

    commit 2

--- A.cpp
+++ A.cpp
@@ -1,9 +1,23 @@
 #include "A.h"
 
+static void Foo(void) {
+
+}
+
 int main() {
     bool flag = true;
     if(flag) {
         // Do something
+        Foo();
+        Foo();
+        Foo();
+        Foo();
+        Foo();
+        Foo();
+        Foo();
+        Foo();
+        Foo();
+        Foo();
     }
     else {
         // Do other things
